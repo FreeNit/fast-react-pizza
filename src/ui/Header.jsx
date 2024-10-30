f7e5c7c58832502119743366917f41045a8ab3aa
Dimon <bolilui.dmytro@gmail.com>
2024-10-30 13:14:42 +0200

    Styled some components with Tailwind

---
+++ src/ui/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/user/SearchOrder";
+import Username from "../features/user/Username";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+
+      <SearchOrder />
+
+      <Username />
+    </header>
+  );
+}
